9b86c7810594864aef4ba62e3c1097910886ff18
Iriseplosc <42510470+creamIcec@users.noreply.github.com>
2023-11-19 23:51:17 +0800

    basically redesigned UI

--- vue-visualplatform3d/src/App.vue
+++ vue-visualplatform3d/src/App.vue
@@ -35,15 +35,15 @@ export default{
 </script>
 
 <template>
-  <Header title="无人机3D实时监控平台"/>
+  <Header title="智能巡检实时3D监控平台"/>
   <div id="content">
     <div id="map-content"></div> <!--地图内容--->
     <div id="widget-content"> <!--非地图内容-->
       <div id="left-area">
-        <Button id="navigator" action="back"></Button>  <!--返回按钮-->
-        <Button id="display-control" action="control path"></Button>  <!--显示元素的控制按钮-->
-        <Button id="menu-control" action="menu"></Button>  <!--展开菜单控制-->
-        <Button id="timeline-control" action="timeline"></Button>  <!--时间线控制-->
+        <Button id="navigator" action="back" icon="src/resources/back.svg"></Button>  <!--返回按钮-->
+        <Button id="display-control" action="control path" icon="src/resources/path.svg"></Button>  <!--显示元素的控制按钮-->
+        <Button id="menu-control" action="menu" icon="src/resources/menu.svg"></Button>  <!--展开菜单控制-->
+        <Button id="timeline-control" action="timeline" icon="src/resources/timeline.svg" style="grid-row: 7;"></Button>  <!--时间线控制-->
       </div>
       <VideoCard/> <!--即时视频区域--->
       <TrackingCard/> <!--TODO: vue的自定义元素内容方法-->
@@ -73,12 +73,31 @@ export default{
     pointer-events: none;
 }
 #left-area{
-    width: 100px;
-    height: 100%;
+    width: 90px;
+    height: 80%;
+    margin-top: 1%;
+    margin-left: 1%;
     display: grid;
     grid-template-columns: 100%;
-    grid-template-rows: repeat(8, 120px) calc(100% - 800px);
+    grid-template-rows: repeat(8, 100px) calc(100% - 700px);
     justify-items: center;
     align-items: center;
+
+    background: linear-gradient(135deg, var(--controller-color-start), var(--controller-color-end)),
+                linear-gradient(270deg, var(--controller-color-end) 20%, var(--controller-color-start) 80%);
+    border-radius: 20px;
+    border: 1px solid rgba(255,255,255,0.3);
+    backdrop-filter: blur(50px);
+    -webkit-backdrop-filter: blur(50px);
+    box-shadow: 0px 8px 32px 0px rgba(0,0,0,0.37);
+}
+</style>
+<style>
+:root{
+    --header-color-start: #04353f90;
+    --header-color-end: #08155290;
+    --controller-color-start: #2ab8d460;
+    --controller-color-end: #21ad7a60;
+    --night-font-color: #ffffff;
 }
 </style>
--- vue-visualplatform3d/src/components/VideoCard.vue
+++ vue-visualplatform3d/src/components/VideoCard.vue
@@ -12,8 +12,15 @@
     position: absolute;
     right: 20px;
     top: 20px;
-    background-color: #031b5150;
     border-radius: 14px;
     overflow: hidden;
+
+    background: linear-gradient(135deg, var(--header-color-start), var(--header-color-end)),
+                linear-gradient(270deg, var(--header-color-end) 20%, var(--header-color-start) 80%);
+    border-radius: 20px;
+    border: 1px solid rgba(255,255,255,0.3);
+    backdrop-filter: blur(50px);
+    -webkit-backdrop-filter: blur(50px);
+    box-shadow: 0px 8px 32px 0px rgba(0,0,0,0.37);
 }
 </style>
